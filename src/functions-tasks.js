846d79bb0f28d14ebb7b86a6d362102c999aefbd
Dastan Hairushev <hairushevdastan@gmail.com>
2026-01-11 22:24:06 +0500

    feat: 6 passing, 5 pending

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -115,8 +115,15 @@ function getPolynom(...coefficients) {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  let memoized = null;
+
+  return () => {
+    if (memoized === null) {
+      memoized = func();
+    }
+    return memoized;
+  };
 }
 
 /**

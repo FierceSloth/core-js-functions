704c442ad3d480cbcdd6d76f4663c432329b078e
Dastan Hairushev <hairushevdastan@gmail.com>
2026-01-12 18:21:33 +0500

    feat: 11 passing, 0 pending

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -204,8 +204,10 @@ function logger(func, logFunc) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args) {
+  return (...otherArg) => {
+    return fn(...args, ...otherArg);
+  };
 }
 
 /**
@@ -225,8 +227,13 @@ function partialUsingArguments(/* fn, ...args */) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let id = startFrom;
+  return () => {
+    const result = id;
+    id += 1;
+    return result;
+  };
 }
 
 module.exports = {
